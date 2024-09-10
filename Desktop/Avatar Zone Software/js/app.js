cf4b4cda829ee25cb417660ec3571424c618167c
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-09-10 13:20:02 +0400

    Delete Desktop directory

--- Desktop/Avatar Zone Software/js/app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('listFiltersBtn').addEventListener('click', function() {
-    document.getElementById('filtersAside').classList.add('show');
-});
-
-document.getElementById('closeAsideBtn').addEventListener('click', function() {
-    document.getElementById('filtersAside').classList.remove('show');
-});
--- Desktop/flask/static/JS/showComment.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.toggle-comments').forEach(function(element) {
-    element.addEventListener('click', function() {
-        var container = this.nextElementSibling;
-        container.classList.toggle('active');
-    });
-});
